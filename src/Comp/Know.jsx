de7a81e0283ec96aaea097e7e4ad2323f8a93a93
Thando Nkala <nkalathando01@gmail.com>
2025-07-27 12:09:17 -0700

    me

--- src/Comp/Know.jsx
+++ src/Comp/Know.jsx
@@ -1,9 +1,9 @@
 
 function Know() {
   return (
-      <section className='h-screen bg-[url("/Us.png")] bg-contain bg-center md:bg-no-repeat'>
-    <div className='h-screen bg-gradient-to-r from-[#512842]/70 to-[#461B36]/0 w-full'>
-          <div className=' w-80 absolute md:bottom-[30px] bottom-[250px] md:left-10 left-10 flex flex-col justify-center items-center'>
+      <section className='md:h-screen h-47 bg-[url("/Us.png")] bg-contain bg-center bg-no-repeat'>
+    <div className='md:h-screen h-47 bg-gradient-to-r from-[#512842]/70 to-[#461B36]/0 w-full'>
+          <div className='md:block hidden  w-80 absolute md:bottom-[30px] bottom-[250px] md:left-10 left-10 flex flex-col justify-center items-center'>
             <h1 className='text-white text-2xl italic font-playfair'>Know us Better</h1>
             <h1 className='text-white text-6xl font-playfair'>About Us</h1>
             </div>
