ba145639e2e08561f7d738c2e3d5487f1b5e3fa6
Thando Nkala <nkalathando01@gmail.com>
2025-07-28 01:48:14 -0700

    mecca

---
+++ src/Comp/Being.jsx
@@ -0,0 +1,33 @@
+function Being (){
+    return(
+         <>
+      <section className="container mx-auto flex flex-wrap justify-between items-center font-playfair ">
+        <div className="flex-col justify-between items-center w-161 h-130">
+          <h1 className="md:text-4xl text-2xl font-bold text-[#461B36]  m-6">
+            100% of our results are <br />
+            obtained within a clearly <br />
+            defined time frame, even <br />
+            from scratch.
+          </h1>
+          <p className="text-[#461B36]  m-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam{" "}
+            <br />
+            sollicitudin est in rutrum sagittis. Fusce eu mi vehicula lacus
+            rutrum <br />
+            fringilla. Mauris vel suscipit felisLorem{" "}
+          </p>
+
+          <button className=" m-6 text-white cursor-pointer bg-gradient-to-r from-[#461B36] to-pink-500 rounded p-2">
+            Enroll now
+          </button>
+
+       
+        </div>
+     
+      </section>
+    </>
+    )
+}
+
+
+export default Being;
--- src/Pages/Newsroom.jsx
+++ src/Pages/Newsroom.jsx
@@ -1,11 +1,9 @@
+import Being from '../Comp/Being'
+
+
 function Newsroom() {
   return (
-     <div className='h-200 '>
-
-
-        <h1 className="text-[#461B36] m-5 text-2xl font-playfair">Coming Soon</h1>
-
-    </div>
+   <Being/>
   )
 }
 
